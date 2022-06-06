f205053e348eb106a625b14165a68e8c34587661
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-06 15:21:37 -0400

    Implemented DOM update functionality

--- script.js
+++ script.js
@@ -26,3 +26,17 @@ async function getRandomUser() {
 function addData(object) {
     data.push(object);
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+  
+    providedData.forEach(item => {
+        const element = document.createElement("div");
+        element.classList.add("person");
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        
+        main.appendChild(element);
+    });
+}
